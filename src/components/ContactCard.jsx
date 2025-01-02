69803551417c906a583f96e51c7fc79d0b2d3a1f
coriolis110801 <zhenyuli_zyl@outlook.com>
2025-01-02 21:07:41 +0000

    查看

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import { faEdit, faTrash, faUser } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
+import { faEdit, faTrash, faUser, faEye } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 import styles from '../style/ContactCard.module.css';
-import { Flex, Typography } from 'antd';
+import { Flex, Typography, Modal, Button } from 'antd';
 
 const ContactCard = ({ contact, handleEdit, deleteContact }) => {
+  const [isViewModalOpen, setIsViewModalOpen] = useState(false);
+
   const updateHandler = (id) => {
     handleEdit(id);
   };
 
   const deleteContactHandler = (id) => {
     deleteContact(id);
+  };
+
+  const openViewModal = () => {
+    setIsViewModalOpen(true);
+  };
+
+  const closeViewModal = () => {
+    setIsViewModalOpen(false);
   };
 
   return (
@@ -41,6 +51,9 @@ const ContactCard = ({ contact, handleEdit, deleteContact }) => {
       </Link>
 
       <Flex align="center">
+        <div className={styles.mr4} onClick={openViewModal}>
+          <FontAwesomeIcon size="2x" icon={faEye} />
+        </div>
         <div className={styles.mr4} onClick={() => updateHandler(contact.id)}>
           <FontAwesomeIcon size="2x" icon={faEdit} />
         </div>
@@ -48,6 +61,20 @@ const ContactCard = ({ contact, handleEdit, deleteContact }) => {
           <FontAwesomeIcon size="2x" icon={faTrash} />
         </div>
       </Flex>
+
+      {/* View Contact Modal */}
+      <Modal
+        title="View Contact"
+        open={isViewModalOpen}
+        onCancel={closeViewModal}
+        footer={[
+          <Button key="close" onClick={closeViewModal}>
+            Close
+          </Button>,
+        ]}
+      >
+        <Typography.Text>查看功能 coming soon... please wait</Typography.Text>
+      </Modal>
     </Flex>
   );
 };
