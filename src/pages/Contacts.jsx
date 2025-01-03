129c68c0239014e05d7ce9e6b4a78708b77cf43c
coriolis110801 <zhenyuli_zyl@outlook.com>
2025-01-03 09:33:10 +0000

    路线清单

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -179,6 +179,17 @@ const Contacts = () => {
         </div>
         {/* Add New Buttons Below Sort By Dropdown */}
         <div className={styles.pContent}>
+          <Select
+            placeholder="路线清单"
+            style={{ width: '100%' }}
+            onChange={(value) => console.log('Selected:', value)}
+          >
+            <Option value="option1">Option 1</Option>
+            <Option value="option2">Option 2</Option>
+            <Option value="option3">Option 3</Option>
+          </Select>
+        </div>
+        <div className={styles.pContent}>
           <Button
             type="default"
             block
